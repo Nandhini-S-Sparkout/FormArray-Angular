78c5c210a21045bc0b9d361f82ff2146da67d680
Nandhini-S-Sparkout <nandhini@sparkouttech.com>
2024-05-14 10:04:13 +0530

    FormArray

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+myForm!:FormGroup;
+constructor()
+{
+
+}
+
+ngOnInit(){
+  this.myForm=new FormGroup({
+    'itemName':new FormControl(''),
+    'subCategoryName':new FormArray([])
+  })
+}
+
+get itemControls()
+{
+  return (<FormArray>this.myForm.get('subCategoryName')).controls;
+}
+onAddItems(){
+     const itemsName=this.myForm.get('itemName')?.value.trim();
+     if(itemsName !== '')
+    {
+      const itemNameControl=new FormControl(itemsName,Validators.required);
+      (this.myForm.get('subCategoryName') as FormArray).push(itemNameControl);
+      console.log(itemNameControl);
+      
+      this.myForm.get('itemName')?.setValue('');
+    }
+
+  }
+
+  onRemoveItem(i:number)
+  {
+    (this.myForm.get('subCategoryName') as FormArray).removeAt(i);
+  }
+
+}
